7f84a37d7398962c3b79b72b26966a1698312f91
Ceo-Sammarco <noah.kd@gmail.com>
2025-12-08 09:23:06 -0300

    @Ceo-Sammarco - 08/12/2025

--- index.jsx
+++ index.jsx
@@ -8,6 +8,36 @@ const data = {
   ps4SystemUpdates: "https://archive.midnightchannel.net/SonyPS/Firmware/?cat=ps4sys#google_vignette",
   apps: "pkg-zone.com",
 };
+const games = {
+  residentEvil: [
+    {
+      codeVeronica: {
+        id: "Resident Evil Code Veronica X - 2017",
+        title: '',
+        v: '',
+        download: 'https://akirabox.to/BnkmWVw60GR0/file',
+        file: 'R_07104_v1.00-v1.01_[4.00]_OPOISSO893-[DLPSGAME.COM].rar',
+      },
+      codeVeronica2001: {
+        id: '',
+        download: 'https://www.mediafire.com/file/z7gx3rtddk6brlp/UP9000-SLUS22184_00-SLUS201840000001-A0100-V0100.pkg/file',
+        file: 'UP9000-SLUS22184_00-SLUS201840000001-A0100-V0100.pkg'
+      },
+      residentEvil2Remake: {
+        id: 'CUSA09193– USA',
+        title: 'Resident Evil 2 Remake Deluxe Edition',
+        v: '1.07',
+        page: 'https://dlpsgame.com/resident-evil-2-ps4-pkg/',
+        download: [
+          'https://www.mediafire.com/file/ph3if2henly7ivq/R2_09193-[+PS4+-+PS5+].part1.rar/file',
+          'https://www.mediafire.com/file/2a80k60lmzh858o/R2_09193-[+PS4+-+PS5+].part2.rar/file',
+          'https://www.mediafire.com/file/u0v9sko2xe07333/R2_09193-[+PS4+-+PS5+].part3.rar/file',
+        ],
+        languages: 'Eng, Fra, Spa, Ger, Ita, Jpn, (text: Rus, Pol, Por/Bra)',
+      },
+    },
+  ],
+};
 
 function Text( { children, as = "span", ...props } ) {
    const Element = as;
